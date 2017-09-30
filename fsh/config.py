cf760c8864ba6cc6e2ef6054c009d3e5b98242e0
Adrian Schneider <leftshiftlp@gmail.com>
2017-09-30 23:05:00 +0200

    set global paths

--- fsh/config.py
+++ fsh/config.py
@@ -1,5 +1,5 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:////var/fsh/test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-UPLOAD_FOLDER = '/tmp'
+UPLOAD_FOLDER = '/var/fsh/files'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
